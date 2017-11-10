ca651c504c8bc25d429104bb6b2579ecf48e4d60
Kaoru1126 <chocolate.k1126@gmail.com>
2017-11-10 12:21:05 +0900

    changed

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '172.31.28.26', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
@@ -60,4 +60,4 @@
 #     # password: "please use keys"
 #   }
 
-server '13.114.233.161', user: 'ec2-user', roles: %w{app db web}
+
